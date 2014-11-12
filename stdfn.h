ed2d683939bffe0f0dff5aaa7879e9b8a839a04c
sasha <spavelovich@lexipedium.org>
2014-11-12 01:33:42 -0500

    Making change so the header works

--- stdfn.h
+++ stdfn.h
@@ -1,12 +1,13 @@
 #ifndef STDFN_H
 #define STDFN_H
+#include "stdfn.c"
 int intro(char name[30], char author[40], int year, char license[10]);
 int askInt(char question[200]);
 char *askChar(char *question);
 double askDouble(char question[200]);
-int asdfwozxcpekvsdwri_gRan1 = 1; // Making sure that no one will accidently use these variables
-int asdfwozxcpekvsdwri_gRan2 = 1;
-int asdfwozxcpekvsdwri_gRan3 = 1;
+int asdfwozxcpekvsdwri_gRan1; // Making sure that no one will accidently use these variables
+int asdfwozxcpekvsdwri_gRan2;
+int asdfwozxcpekvsdwri_gRan3;
 int ranGen(void);
 int busyRun(int runs);
 char printOut(int rate, char *string);
